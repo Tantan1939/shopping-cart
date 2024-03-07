9cde2c474f8c7f32eb73d790cf69bf7761553a75
Tantan1939 <101438750+Tantan1939@users.noreply.github.com>
2024-03-07 16:44:52 +0800

    add tags

---
+++ Shopping-cart/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shopping_cart.Controllers
+{
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+
+        [Route("[action]")]
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [Route("[action]")]
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [Route("[action]")]
+        [HttpGet]
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
